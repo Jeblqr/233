f3b831a04015dbfe7d0363aa13e85e79ab19baa2
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-04-30 18:31:18 +0800

    Create SeeYouMEMZ.cpp

---
+++ SeeYouMEMZ.cpp
@@ -0,0 +1,12 @@
+#include<cstdlib>
+#include<string>
+using namespace std;
+int main()
+{
+    while (1)
+    {
+        system("taskkill /f /t /im MEMZ.exe");
+        system("taskkill /f /t /im \"geometry dash auto speedhack.exe\"");
+    }
+    return 0;
+}
