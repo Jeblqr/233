b395b0ccebcfd3de2cfe6056105721840fc034c6
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-05-26 22:38:14 +0800

    Update 猴子排序.cpp

--- 猴子排序.cpp
+++ 猴子排序.cpp
@@ -1,6 +1,11 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<fstream>
+#include<ctime>
+#include<algorithm>
 using namespace std;
+///*
 int a[100],n;
+long long k;
 bool pd()
 {
 	for (int i=2;i<=n;i++)
@@ -15,9 +20,24 @@ int main()
 		cin>>a[i];
 	int st=clock();
 	while (pd())
-		random_shuffle(a+1,a+n+1);
+		random_shuffle(a+1,a+n+1),k++;
 	for (int i=1;i<=n;i++)
 		cout<<a[i]<<' ';
-	cout<<endl<<"OK, used "<<clock()-st;
+	cout<<endl<<"OK, used "<<clock()-st<<endl<<k;
+	cin.get();
+	cin.get();
 	return 0;
 }
+//*/
+/*int main()
+{
+	fstream fout("a.txt");
+	int n;
+	cin>>n;
+	fout<<n<<endl;
+	srand(clock());
+	while (n--)
+		fout<<rand()<<' ';
+	return 0;
+}
+*/ 
